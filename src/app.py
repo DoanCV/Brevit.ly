36d4d584f0465833032b636c9e3b14d336163069
Brian <briandoan07@gmail.com>
2021-06-20 22:42:55 -0400

    started redis session setup

--- src/app.py
+++ src/app.py
@@ -6,10 +6,20 @@ import redis
 # generator.py to generate new link
 import generator
 
+# startup FLask app
 app = Flask(__name__)
+
+# startup NoSQL DB
+redis_session = redis.Redis(host = "localhost", port = 5000, db = 0)
+
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def hello_world():
     return "Hello world!"
 
+if __name__ == '__main__':
+    app.run()
 
